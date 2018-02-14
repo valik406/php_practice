6f2011ba51f16d3200282c91967c74881ebe4027
valentyn <valik406@gmail.com>
2018-02-14 10:47:19 +0200

    Revert "Создание класа Router"
    This reverts commit a1a2327b463427761fdb36cb3f3d02a8c5b9efbf.

--- index.php
+++ index.php
@@ -8,8 +8,7 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 // 2. Подключения файлов системы
-define('ROOT', dirname(__FILE__));
-require_once(ROOT . '/components/Router.php');
+
 
 // 3. Установка соединения с БД
 
