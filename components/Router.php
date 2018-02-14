3b592679ec52c1992d0875f41b8ebbc16677a713
valentyn <valik406@gmail.com>
2018-02-14 11:36:46 +0200

    Получение строки запроса

--- components/Router.php
+++ components/Router.php
@@ -9,7 +9,27 @@ class Router {
         $this->routes = include($routesPath);
     }
     
+    /**
+     * Return request string
+     */
+    private function getURI() {
+        if(!empty($_SERVER['REQUEST_URI'])){
+            return trim($_SERVER['REQUEST_URI'], '/');
+        }
+    }
+    
     public function run() {
-        print_r($this->routes);
+        // Получить строку запроса
+        $uri = $this->getURI();
+        echo $uri;
+        // Проверить наличие такого запроса в routes.php
+        
+        // Если есть совпадение, опредилить какой контроллер
+        // и action обрабатывают запрос
+        
+        //подключить файл класса контроллера
+        
+        //Создать обект вызвать метод(т.е. action)
+        
     }
 }
