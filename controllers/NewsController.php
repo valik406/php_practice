b3c590f186f040619c02f1a90ec15b19dc44fe59
valentyn <valik406@gmail.com>
2018-02-14 12:22:23 +0200

    Создания контроллеров

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,8 +1,11 @@
 <?php
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+class NewsController{
+    
+    
+    public function actionIndex() {
+        echo 'NewsController actionIndex()';
+        return true;
+    }
+}
 
