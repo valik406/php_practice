c493ceba2dbe1034c199e8d034bd0b60a602b405
valentyn <valik406@gmail.com>
2018-02-15 11:35:09 +0200

    Изменения разбора получение одной новости

--- config/routes.php
+++ config/routes.php
@@ -1,6 +1,6 @@
 <?php
 return array(
-    'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2',
+    'news/([0-9]+)' => 'news/view/$1',
     'news' => 'news/index',
     'products' => 'product/list',
     'radio' => 'radio/index'
