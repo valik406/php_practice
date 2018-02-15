fb4adf8af49e16e493a1d92b6b04d005df697a78
valentyn <valik406@gmail.com>
2018-02-15 10:10:03 +0200

    Создание файлов моделей

---
+++ models/News.php
@@ -0,0 +1,21 @@
+<?php
+
+class News{
+    /**
+     * Returns single news item with specified id
+     * @param integer $id
+     */
+    public static function getNewsItemById($id){
+        
+        
+    }
+    
+    /**
+     *  Return an array of news items
+     */
+    public static function getNewsList(){
+        
+        
+    }
+}
+
