b273ec9e74cc70489c5e760c22bca88a7d0d3325
valentyn <valik406@gmail.com>
2018-02-15 11:34:07 +0200

    Получение информации по новостям с базы данных

--- controllers/NewsController.php
+++ controllers/NewsController.php
@@ -1,10 +1,29 @@
 <?php
-
+include_once ROOT . '/models/News.php';
 class NewsController{
     
     
     public function actionIndex() {
-        echo 'NewsController actionIndex()';
+        $newsList = [];
+        $newsList = News::getNewsList();
+        
+        echo '<pre>';
+        print_r($newsList);
+        echo '</pre>';
+        return true;
+    }
+    
+    public function actionView($id) {
+        if($id):
+            $newsItem = News::getNewsItemById($id);
+            echo '<pre>';
+            print_r($newsItem);
+            echo '</pre>';
+        endif;
+        
+        echo 'Просмотр Одной новости'.'<br>';
+
+        echo $id.'<br>';
         return true;
     }
 }
