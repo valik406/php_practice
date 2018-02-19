f82d09d9f3a9e371122b9838443ec4dd1faa774f
valentyn <valik406@gmail.com>
2018-02-19 15:06:22 +0200

    Добавления маршрута для постраничной навигации и маршрута для регистрации

--- config/routes.php
+++ config/routes.php
@@ -4,7 +4,9 @@ return array(
     'radio' => 'radio/index',
     'product/([0-9]+)' => 'product/view/$1',
     'catalog' => 'catalog/index',
+    'category/([0-9]+)/page-([0-9]+)' => 'catalog/category/$1/$2',
     'category/([0-9]+)' => 'catalog/category/$1',
+    'user/register' => 'user/register',
     '' => 'site/index'
 );
 
