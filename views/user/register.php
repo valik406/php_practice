681631e849b06ed2de6cf66b090eae6df6fb6ff4
valentyn <valik406@gmail.com>
2018-02-19 15:19:51 +0200

    Форма регистрации

---
+++ views/user/register.php
@@ -0,0 +1,32 @@
+<?php include ROOT . '/views/layouts/header.php'; ?>
+<section><!--form-->
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-4 col-sm-offset-4 padding-right">
+                <?php if ($result): ?>
+                    <p>Вы зарегестрированы!</p>
+                <?php else: ?>
+                    <?php if (isset($errors) && is_array($errors)): ?>
+                        <ul style="color: red">
+                            <?php foreach ($errors as $error): ?>
+                                <li>- <?= $error ?></li>
+                            <?php endforeach; ?>
+
+                        </ul>
+                    <?php endif; ?>
+                    <div class="signup-form"><!--sign up form-->
+                        <h2>Регистрация на сайте</h2>
+                        <form action="" method="post">
+                            <input type="text" name="name" placeholder="Имя" value="<?= $name ?>"/>
+                            <input type="email" name="email" placeholder="E-mail" value="<?= $email ?>"/>
+                            <input type="password" name="password" placeholder="Пароль" value="<?= $password ?>"/>
+                            <input type="submit" name="submit" class="btn btn-default" value="Регистрация"/>
+                        </form>
+                    </div><!--/sign up form-->
+                <?php endif; ?>
+            </div>
+        </div>
+    </div>
+</section><!--/form-->
+<?php include ROOT . '/views/layouts/footer.php'; ?>
+
